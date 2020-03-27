4d137bdd94c0b413804e90737f81229e1c8e996e
Aleksandr Botškarjov <aleksandrbotskarjov@gmail.com>
2020-03-27 22:33:34 +0200

    new clas Measure to Domain/Quantity

---
+++ Domain/Quantity/Measure.cs
@@ -0,0 +1,23 @@
+﻿using Abc.Data.Quantity;
+using Abc.Data.Common;
+
+namespace Abc.Domain.Quantity
+{
+   public class Measure : Entity<MeasureData>
+
+    {
+        public Measure(MeasureData data) : base(data)
+        {
+        }
+    }
+
+   public abstract class Entity<T> where T : PeriodData
+   {
+        public T Data { get; }
+
+        protected Entity(T data)
+        {
+            Data = data;
+        }
+   }
+}
