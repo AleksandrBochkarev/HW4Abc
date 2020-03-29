cc31124751315832f00b304e6468bfddbc1a4f5c
Aleksandr Botškarjov <aleksandrbotskarjov@gmail.com>
2020-03-29 13:52:55 +0300

    quantityDbContext update

--- Infra/Quantity/QuantityDbContext.cs
+++ Infra/Quantity/QuantityDbContext.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Abc.Data.Quantity;
+using Microsoft.EntityFrameworkCore;
 
 namespace Abc.Infra.Quantity
 {
-    public class QuantityDbContext
+    public class QuantityDbContext: DbContext
     {
-    }
+
+            public QuantityDbContext(DbContextOptions<QuantityDbContext> options)
+                : base(options)
+            {
+            }
+            public DbSet<MeasureData> Measures { get; set; }
+
+            protected override void OnModelCreating(ModelBuilder builder)
+            {
+                base.OnModelCreating(builder);
+                InitializeTables(builder);
+            }
+
+            protected static void InitializeTables(ModelBuilder builder)
+            {
+                builder.Entity<MeasureData>().ToTable(nameof(Measures));
+        }
+        }
 }
